29bbdc0141f9c128f557c00d4d17153987c0ae9e
Jacob Tyler <jacob.tyler@abcam.com>
2022-06-22 14:13:09 +0100

    Updated MapReduce Class

--- do_map_reduce.py
+++
@@ -1,9 +0,0 @@
-
-mapReducer = MapReduce()
-
-def mapper(record):
-    #Start writing the Map code here
-
-def reducer(key, list_of_values):
-    #Start writing the Reduce code here
-
--- mapper_reducer.py
+++ mapper_reducer.py
@@ -1,30 +1,35 @@
 from collections import OrderedDict
+from collections.abc import Callable
+from typing import Union
+
+DataKey = Union[str, int]
+DataValue = Union[str, int]
+
+MapperFunction = Callable[[object], None]
+ReducerFunction = Callable[[object, DataKey], None]
 
 
 class MapReduce:
     def __init__(self):
-      self.initial = {}
-      self.intermediate = OrderedDict()
-      self.result = []
+        self.initial = {}
+        self.intermediate = OrderedDict()
+        self.result = []
 
     def set_initial(self, data: dict):
-      self.initial = data
+        self.initial = data
 
-    def store_intermediate(self, key, value):
-      self.intermediate.setdefault(key, [])
-      self.intermediate[key].append(value)
+    def store_intermediate(self, key: DataKey, value: DataValue):
+        self.intermediate.setdefault(key, [])
+        self.intermediate[key].append(value)
 
-    def store_result(self, value):
-      self.result.append(value)
+    def store_result(self, value: DataValue):
+        self.result.append(value)
 
-    def execute(self, mapper: callable, reducer: callable):
-      for record in data:
-        mapper(record)
+    def execute(self, mapper: MapperFunction, reducer: ReducerFunction):
+        for record in self.initial:
+            mapper(record)
 
-      for key in self.intermediate:
-        reducer(key, self.intermediate[key])
+        for key in self.intermediate:
+            reducer(key, self.intermediate[key])
 
-      self.result.sort()
-        
-
-
+        self.result.sort()
