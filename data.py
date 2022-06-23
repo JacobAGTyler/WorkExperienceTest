e1a975bf75e390c920c1e9b5b6d7e63cf8675cab
Jacob Tyler <jacob.tyler@abcam.com>
2022-06-23 13:33:19 +0100

    Data Persistence Improvements

--- data.py
+++ data.py
@@ -1,17 +1,27 @@
 from replit import db
+from dataclasses import dataclass, asdict
 
 
-def save_obj(obj: object) -> bool:
-    try:
-        if obj.id is None:
-            raise AttributeError
-    except AttributeError as e:
-        print(e)
-        return False
+def save_obj(obj: dataclass) -> bool:
+    if obj.id is None:
+        raise AttributeError
 
-    key = f'dc_{obj.__name__}_{obj.id}'
-    value = dict(obj)
+    key = f'dc_{obj.__class__.__name__}_{obj.id}'
+    value = asdict(obj)
 
     db[key] = value
 
     return True
+
+
+def list_objects(retrieval_class):
+    prefix = f'dc_{retrieval_class.__name__}'
+    matches = db.prefix(prefix)
+
+    object_list = []
+    match: dict
+    for match in matches:
+        match['id'] = int(match['id'].replace(f'{prefix}_', ''))
+        object_list.append(retrieval_class(**match))
+
+    return object_list
--- test_data.py
+++ test_data.py
@@ -1,7 +1,18 @@
 import pytest
 
 from dataclasses import dataclass
-from data import save_obj
+from data import save_obj, list_objects
+from replit.database import Database
+from unittest import mock
+
+
+@pytest.fixture
+def fake_db() -> Database:
+    MockDB = mock.Mock(Database)
+    fake_db = MockDB()
+    fake_db.__setitem__ = mock.MagicMock()
+    fake_db.prefix = mock.MagicMock(return_value=[{'id': 'dc_FixtureClass_1', 'name': 'Jacob', 'number': 42}])
+    return fake_db
 
 
 @dataclass
@@ -11,9 +22,14 @@ class FixtureClass:
     number: int
 
 
-def test_save_obj():
-    fc = FixtureClass(id=1, name='Jacob', number=42)
-    result = save_obj(fc)
+@pytest.fixture
+def fixture_instance() -> FixtureClass:
+    return FixtureClass(id=1, name='Jacob', number=42)
+
+
+def test_save_obj(monkeypatch: pytest.MonkeyPatch, fake_db, fixture_instance):
+    monkeypatch.setattr('data.db', fake_db)
+    result = save_obj(fixture_instance)
 
     assert result is True
 
@@ -24,9 +40,18 @@ class IdFreeFixtureClass:
     number: int
 
 
-def test_save_id_free_save_obj():
+def test_save_id_free_save_obj(monkeypatch: pytest.MonkeyPatch, fake_db):
+    monkeypatch.setattr('data.db', fake_db)
     id_free = IdFreeFixtureClass(number=43, name='Jacob')
 
     with pytest.raises(AttributeError):
         result = save_obj(id_free)
         assert result is False
+
+
+def test_list_objects(monkeypatch: pytest.MonkeyPatch, fake_db):
+    monkeypatch.setattr('data.db', fake_db)
+    lst = list_objects(FixtureClass)
+
+    assert type(lst) == list
+    assert lst == [FixtureClass(id=1, name='Jacob', number=42)]
